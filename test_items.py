ea4b83f1e1a36b952b5221554a28a1ea61f595d4
Marina Dubrovskaya <kouznetsovasd7@gmail.com>
2022-09-05 10:46:09 +0300

    test file commit is checked

--- test_items.py
+++ test_items.py
@@ -2,15 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import math
-import pytest
-from selenium.webdriver.support.ui import Select
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_guest_should_see_login_link(browser):
-    #try:
-        browser.get(link)
-        button = len(browser.find_elements(By.CLASS_NAME, "btn btn-lg btn-primary"))
-        assert button > 0, '!!!НЕ НАШЕЛ!!!'
+def test_availability_of_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    button = len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket"))  
+    assert button > 0, "Button is not found!"
